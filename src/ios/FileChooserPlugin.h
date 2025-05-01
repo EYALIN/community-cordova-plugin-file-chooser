3cdc8dd9a517b0d706e7dc299b6538af8ff8d084
eyalin <yalin.eliad@gmail.com>
2025-05-01 22:33:23 +0300

    fixed ios version

--- src/ios/FileChooserPlugin.h
+++ src/ios/FileChooserPlugin.h
@@ -1,6 +1,9 @@
 #import <Cordova/CDV.h>
 
-@interface FileChooserPlugin : CDVPlugin
+@interface FileChooserPlugin : CDVPlugin <UIDocumentPickerDelegate>
+
+@property (nonatomic, strong) NSString* callbackId;
+@property (nonatomic, strong) NSDictionary* options;
 
 - (void)chooseFile:(CDVInvokedUrlCommand*)command;
 
